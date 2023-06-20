e07bf0620c428bda1e769c1e813992978e15f128
mino <qawsedrfnnn@gmail.com>
2023-06-20 16:45:59 +0900

    login,register Form html & Devtools build.gradle 추가 + Livereload 설정

--- src/main/java/com/travel/web_oasis/controller/MemberController.java
+++ src/main/java/com/travel/web_oasis/controller/MemberController.java
@@ -1,8 +1,19 @@
 package com.travel.web_oasis.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/user")
+@Controller
+@RequestMapping("/member")
 public class MemberController {
+    @GetMapping("/login")
+    public String loginForm() {
+        return "/member/loginForm";
+    }
 
+    @GetMapping("/register")
+    public String registerForm() {
+        return "/member/registerForm";
+    }
 }
