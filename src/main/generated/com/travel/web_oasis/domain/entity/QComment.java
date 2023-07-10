4502cea0fa85b54e9ffe7c48b3cf3af7ecf8e34e
industry1111 <industry1111@naver.com>
2023-07-11 01:18:01 +0900

    프로필수정화면 타임변환 부분 수정

---
+++ src/main/generated/com/travel/web_oasis/domain/entity/QComment.java
@@ -0,0 +1,53 @@
+package com.travel.web_oasis.domain.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QComment is a Querydsl query type for Comment
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QComment extends EntityPathBase<Comment> {
+
+    private static final long serialVersionUID = -1074520695L;
+
+    public static final QComment comment = new QComment("comment");
+
+    public final QBaseEntity _super = new QBaseEntity(this);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdDate = _super.createdDate;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final StringPath modifiedBy = _super.modifiedBy;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedDate = _super.modifiedDate;
+
+    public QComment(String variable) {
+        super(Comment.class, forVariable(variable));
+    }
+
+    public QComment(Path<? extends Comment> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QComment(PathMetadata metadata) {
+        super(Comment.class, metadata);
+    }
+
+}
+
---
+++ src/main/generated/com/travel/web_oasis/web/dto/QMemberDTO.java
@@ -0,0 +1,21 @@
+package com.travel.web_oasis.web.dto;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.ConstructorExpression;
+import javax.annotation.processing.Generated;
+
+/**
+ * com.travel.web_oasis.web.dto.QMemberDTO is a Querydsl Projection type for MemberDTO
+ */
+@Generated("com.querydsl.codegen.DefaultProjectionSerializer")
+public class QMemberDTO extends ConstructorExpression<MemberDTO> {
+
+    private static final long serialVersionUID = 1935512519L;
+
+    public QMemberDTO(com.querydsl.core.types.Expression<Long> id, com.querydsl.core.types.Expression<String> name, com.querydsl.core.types.Expression<String> picture, com.querydsl.core.types.Expression<String> introduction) {
+        super(MemberDTO.class, new Class<?>[]{long.class, String.class, String.class, String.class}, id, name, picture, introduction);
+    }
+
+}
+
